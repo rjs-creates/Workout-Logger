35d5ce5ff41df9ad8397449e11eb63294a3fe290
Rajeshwar Singh <rajeshwars600@gmail.com>
2025-07-06 23:43:32 -0600

    Added Geminiapi

--- backend/services/GeminiApiService.js
+++ backend/services/GeminiApiService.js
@@ -1,28 +1,24 @@
-import axios from 'axios';
+import { GoogleGenAI } from "@google/genai";
 
-export const getGeminiApiResponse = async (prompt) => {
-  try {
-    const response = await axios.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-goog-api-key': `${process.env.GEMINI_API_KEY}`
-      },
-      body: JSON.stringify({
-        prompt: prompt,
-        maxOutputTokens: 100,
-        temperature: 0.7
-      })
-    });
+const ai = new GoogleGenAI({});
 
-    if (!response.ok) {
-      throw new Error(`Error: ${response.statusText}`);
+const PROMPT = `You're a gym trainer, and your task is critque the user's workout sessions
+I will provide you with a workout session, and the end goal that the user wants to achieve.
+You will provide a detailed analysis of the workout session, including: What the user did well, what they could improve, and how they can achieve their goal.
+You will also provide a list of exercises that the user can do to achieve their goal.` + `Here's the workout session:
+{{workoutSession}}` + `The user's goal is: {{goal}}`;
+
+export const callGeminiApi = async(workoutSessionData, goal) => {
+    try {
+        const finalPrompt = PROMPT.replace("{{workoutSession}}", workoutSessionData)
+            .replace("{{goal}}", goal);
+        const response = await ai.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: finalPrompt ,
+        });
+        console.log(response.text);
+    } catch (error) {
+        console.error("Error generating content:", error);
     }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching Gemini API response:', error);
-    throw error;
-  }
 }
+
